6308689f8cbe0df3113422bd8c77299bcec1df1a
lRaphael <107645937+raphagamedev@users.noreply.github.com>
2024-09-10 00:27:19 -0300

    Add files via upload

---
+++ adopet/cadastro.cy.js
@@ -0,0 +1,32 @@
+describe('projeto-adopet', () => {
+    it('passes', () => {
+        cy.visit('https://adopet-frontend-cypress.vercel.app/')
+
+        //#######################################################
+        //              CADASTRANDO USUÁRIO
+        cy.wait(3000);
+        cy.contains('a', 'Cadastrar').click();
+        
+        //-------------------------------------------------------
+        //         incluindo nome
+        cy.get('input[name="nome"]').type('Natália Helena Teixeira');
+        cy.wait(3000);
+        //-------------------------------------------------------
+        //         incluindo e-mail
+        cy.get('input[name="email"]').type('natalia-teixeira74@agaxtur.com.br');
+        cy.wait(3000);
+        //-------------------------------------------------------
+        //         incluindo senha
+        cy.get('input[name="password"]').type('Senha123');
+        cy.wait(3000);
+        //-------------------------------------------------------
+        //         confirma senha
+        cy.get('input[name="confirm_password"]').type('Senha123');
+        cy.wait(3000);
+        //-------------------------------------------------------
+        //         finaliza cadastro
+        cy.contains('button', 'Cadastrar').click();
+
+  
+    })
+  })
